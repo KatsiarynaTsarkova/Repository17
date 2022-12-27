21db2c6c047b2d0ca4bab6eae3d5afe60e10d226
Kate <katya_tsarkov@mail.ru>
2022-12-27 21:21:45 +0300

    work2

---
+++ work2.py
@@ -0,0 +1,7 @@
+#Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z
+
+print('x y z !(x^y)vz')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(f'{x} {y} {z} {int((not x and y) or z)}')
